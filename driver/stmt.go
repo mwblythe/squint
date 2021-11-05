c5c4a9616dd072bccd3c3536bfa5a543c85e5a63
Mark Blythe <stuff@markblythe.com>
2021-11-05 11:35:19 -0700

    StmtExecContext, StmtQueryContext

--- driver/stmt.go
+++ driver/stmt.go
@@ -1,15 +1,16 @@
 package driver
 
 import (
+	"context"
 	"database/sql"
 	"database/sql/driver"
 )
 
 // compile-time interface checks
 var (
-	_ driver.Stmt = (*sqStmt)(nil)
-
-// TODO: StmtExecContext and StmtQueryContext
+	_ driver.Stmt             = (*sqStmt)(nil)
+	_ driver.StmtExecContext  = (*sqStmt)(nil)
+	_ driver.StmtQueryContext = (*sqStmt)(nil)
 )
 
 // sqStmt is a sql.Stmt wrapper to implement the driver.Stmt interface
@@ -22,6 +23,11 @@ func (s sqStmt) Exec(args []driver.Value) (driver.Result, error) {
 	return s.Stmt.Exec(newArgs...)
 }
 
+func (s sqStmt) ExecContext(ctx context.Context, args []driver.NamedValue) (driver.Result, error) {
+	newArgs := namedToIface(args)
+	return s.Stmt.ExecContext(ctx, newArgs...)
+}
+
 func (s sqStmt) NumInput() int {
 	return -1 // no placeholder check
 }
@@ -32,11 +38,26 @@ func (s sqStmt) Query(args []driver.Value) (driver.Rows, error) {
 	return sqRows{r}, err
 }
 
+func (s sqStmt) QueryContext(ctx context.Context, args []driver.NamedValue) (driver.Rows, error) {
+	newArgs := namedToIface(args)
+	r, err := s.Stmt.QueryContext(ctx, newArgs...)
+	return sqRows{r}, err
+}
+
 // convert from []Value to []interface{}
 func valsToIface(in []driver.Value) (out []interface{}) {
 	out = make([]interface{}, len(in))
 	for n := range in {
 		out[n] = in[n]
+	}
+	return
+}
+
+// convert from []NamedValue to []interface{}
+func namedToIface(in []driver.NamedValue) (out []interface{}) {
+	out = make([]interface{}, len(in))
+	for n := range in {
+		out[n] = in[n].Value
 	}
 	return
 }
