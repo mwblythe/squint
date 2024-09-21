7c984a492bb475b7ae87c1925d4f1ec6d080b0e5
Mark Blythe <mblythe@perceptyx.com>
2024-09-20 17:04:14 -0700

    add DB setting wrapper funcs

--- driver/driver.go
+++ driver/driver.go
@@ -7,6 +7,7 @@ import (
 	"database/sql/driver"
 	"errors"
 	"sync"
+	"time"
 
 	"github.com/mwblythe/squint"
 )
@@ -26,7 +27,6 @@ var ErrNoDB = errors.New("no database connection")
 //
 // Name(string) : name to use for the squint driver. (Default "squint-" + toDriver)
 // Builder(*Builder) : squint Builder to use. (Default is Builder with no options)
-//
 func Register(toDriver string, o ...Option) {
 	var drv sqDriver
 	drv.toDriver = toDriver
@@ -52,16 +52,7 @@ type sqDriver struct {
 }
 
 func (d *sqDriver) Open(dsn string) (c driver.Conn, err error) {
-	var db *sql.DB
-	if i, ok := d.db.Load(dsn); ok {
-		db = i.(*sql.DB)
-	} else {
-		db, err = sql.Open(d.toDriver, dsn)
-		if err == nil {
-			d.db.Store(dsn, db)
-		}
-	}
-
+	db, err := d.pool(dsn)
 	if err != nil {
 		return
 	}
@@ -74,8 +65,63 @@ func (d *sqDriver) Open(dsn string) (c driver.Conn, err error) {
 	// build conn
 	conn, err := db.Conn(context.Background())
 	if err == nil && conn != nil {
-		c = newConn(conn, d.builder)
+		c = newConn(conn, d.builder, dsn)
 	}
 
 	return
+}
+
+func (d *sqDriver) pool(dsn string) (db *sql.DB, err error) {
+	if v, ok := d.db.Load(dsn); ok {
+		db = v.(*sql.DB)
+	} else {
+		db, err = sql.Open(d.toDriver, dsn)
+		if err == nil {
+			d.db.Store(dsn, db)
+		}
+	}
+
+	return
+}
+
+func SetConnMaxIdleTime(outerDB *sql.DB, d time.Duration) {
+	inDB, err := innerDB(outerDB)
+	if err == nil && inDB != nil {
+		outerDB.SetConnMaxIdleTime(d)
+		inDB.SetConnMaxIdleTime(d)
+	}
+}
+
+func SetConnMaxLifetime(outerDB *sql.DB, d time.Duration) {
+	inDB, err := innerDB(outerDB)
+	if err == nil && inDB != nil {
+		outerDB.SetConnMaxLifetime(d)
+		inDB.SetConnMaxLifetime(d)
+	}
+}
+
+func innerDB(outerDB *sql.DB) (*sql.DB, error) {
+	driver, ok := outerDB.Driver().(*sqDriver)
+	if !ok {
+		return nil, errors.New("not a squint driver")
+	}
+
+	conn, err := outerDB.Conn(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	var outDB *sql.DB
+	err = conn.Raw(func(driverConn interface{}) error {
+		sqConn, ok := driverConn.(*sqConn)
+		if !ok {
+			return errors.New("not a squint connection")
+		}
+
+		outDB, err = driver.pool(sqConn.dsn)
+		return err
+	})
+
+	return outDB, err
 }
